c20ba118c5a08fe969a8f585f5dc1566f04438e9
David Currie <dcurrie11@qub.ac.uk>
2025-08-07 00:52:21 +0100

    Home dashboard screen UI development with navigation and initial FAB actions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:maintenance_tracker_app/screens/dashboard/dashboard_screen.dart';
+import 'package:maintenance_tracker_app/screens/fab/equipment_onboarding_form_screen.dart';
+import 'package:maintenance_tracker_app/screens/garage/garage_screen.dart';
+import 'package:maintenance_tracker_app/screens/tasks/maintenance_tasks_screen.dart';
+import 'package:maintenance_tracker_app/utils/constants.dart';
+import 'package:path/path.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,26 +19,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: appTitle,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        platform: TargetPlatform.iOS,
+        fontFamily: "WorkSans",
+        textTheme: TextTheme(
+          displayLarge: TextStyle(fontSize: 34),
+          displayMedium: TextStyle(fontSize: 28),
+          displaySmall: TextStyle(fontSize: 20),
+          titleLarge: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.w600,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          titleMedium: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Theme.of(context).colorScheme.onPrimaryContainer,
+          ),
+          labelMedium: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            color: Theme.of(context).colorScheme.onPrimaryContainer,
+          ),
+          labelSmall: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w500,
+            color: Theme.of(context).colorScheme.onPrimaryContainer,
+          ),
+        ),
+        dividerTheme: DividerThemeData(
+          color: Theme.of(context).colorScheme.primary,
+          thickness: 1.5,
+          space: 15,
+        ),
+        iconTheme: IconThemeData(
+          color: Theme.of(context).colorScheme.onPrimaryContainer,
+        ),
+        bottomSheetTheme: BottomSheetThemeData(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          surfaceTintColor: Colors.transparent, // disable M3 tint if you like
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+          ),
+          elevation: 4,
+        ),
+        // TODO: set themes for other widgets
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: appTitle),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: ThemeMode.system,
     );
   }
 }
@@ -54,69 +98,140 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 1;
 
-  void _incrementCounter() {
+  void _onDestinationSelected(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
+  }
+
+  /// Returns the widget for the currently‑selected tab.
+  Widget _getBody() {
+    switch (_selectedIndex) {
+      case 0:
+        return const GarageScreen();
+      case 1:
+        return DashboardScreen(); // existing dashboard content
+      case 2:
+        return const MaintenanceTasksScreen();
+      default:
+        return DashboardScreen();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    void showAddEquipmentForm() {
+      showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
+        builder: (_) => DraggableScrollableSheet(
+          expand: false,
+          initialChildSize: 0.7,
+          maxChildSize: 0.95,
+          minChildSize: 0.4,
+          builder: (context, scrollController) {
+            return Material(
+              textStyle: Theme.of(context).textTheme.bodyMedium,
+              color: Theme.of(context).colorScheme.primaryFixed,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(24),
+              ),
+              child: SingleChildScrollView(
+                controller: scrollController, // ← key line
+                padding: EdgeInsets.fromLTRB(
+                  16,
+                  16,
+                  16,
+                  MediaQuery.of(context).viewInsets.bottom + 16,
+                ),
+                child:
+                    const EquipmentOnboardingFormScreen(), // just the fields & buttons
+              ),
+            );
+          },
+        ),
+      );
+    }
+
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        leading: IconButton(
+          onPressed: null,
+          icon: Icon(Icons.account_circle_sharp),
+          iconSize: 35,
+          color: Theme.of(context).colorScheme.onPrimary,
         ),
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: null,
+            icon: IconButton(
+              onPressed: null,
+              icon: Icon(Icons.settings),
+              iconSize: 35,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _getBody(),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onDestinationSelected,
+        height: 56,
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.warehouse_sharp),
+            label: "Garage",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.dashboard_sharp),
+            label: "Dashboard",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.assignment_sharp),
+            label: "Tasks",
+          ),
+        ],
+      ),
+      floatingActionButton: SpeedDial(
+        icon: Icons.home_repair_service_sharp,
+        activeIcon: Icons.close,
+        spacing: 6,
+        spaceBetweenChildren: 6,
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.add_box_outlined),
+            label: 'Add equipment',
+            onTap: showAddEquipmentForm,
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.timer_outlined),
+            label: 'Log usage',
+            onTap: () {
+              // TODO: implement usage logger
+            },
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.check_circle_outline),
+            label: 'Complete task',
+            onTap: () {
+              // TODO: implement quick-complete
+            },
+          ),
+        ],
+      ),
     );
   }
 }
